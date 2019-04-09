ef538b77d71fdccb14f10af6ebd7359990ca973a
Jang Haemin <io@jhaemin.com>
2019-04-09 17:04:32 +0900

    Separate to a class

---
+++ src/WBStorage.ts
@@ -0,0 +1,52 @@
+import Ruler from "./Ruler"
+
+export default class WBStorage {
+  private currentURL: string
+  private selectedElm: HTMLElement
+  private originalState: {
+    left: number
+    top: number
+    scale: number
+    rotate: number
+  }
+  private finalState: {
+    left: number
+    top: number
+    scale: number
+    rotate: number
+  }
+
+  constructor() {}
+
+  setOriginal(elm: HTMLElement) {
+    this.selectedElm = elm
+    this.originalState.left = elm.getBoundingClientRect().left
+    this.originalState.top = elm.getBoundingClientRect().top
+    this.originalState.scale = Ruler.getScaleXY(elm).x
+    this.originalState.rotate = Ruler.getRotationValue(elm)
+  }
+
+  setFinal() {
+    if (!this.selectedElm) {
+      console.error('No selected element')
+      return
+    }
+
+    this.finalState.left = this.selectedElm.getBoundingClientRect().left
+    this.finalState.top = this.selectedElm.getBoundingClientRect().top
+    this.finalState.scale = Ruler.getScaleXY(this.selectedElm).x
+    this.finalState.rotate = Ruler.getRotationValue(this.selectedElm)
+  }
+
+  getSelectedElement() {
+    return this.selectedElm
+  }
+
+  getOriginalState() {
+    return this.originalState
+  }
+
+  getFinalState() {
+    return this.finalState
+  }
+}
