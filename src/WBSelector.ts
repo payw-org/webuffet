abc121e3f4eda5adf0ba392decb0c0d5bcf16d12
Jang Haemin <io@jhaemin.com>
2019-05-05 22:18:29 +0900

    convert from a single file to a separated class

--- src/WBSelector.ts
+++ src/WBSelector.ts
@@ -1,58 +1,93 @@
-import WBStorage from "./WBStorage"
+import WBSession from "./WBSession"
 
 export default class WBSelector {
   private selectorElm: HTMLElement
-  private readonly wbStorage: WBStorage
+  private readonly wbSession: WBSession
   private mouseMoveTimeout: number
+  private triggerTimeout: number
   private progressBarDom: HTMLElement
-  public isStarted: boolean = false
+  private hoverElm: HTMLElement
+  private isScanCompleteSet: boolean = false
 
-  constructor(selectorElm: HTMLElement, wbStorage: WBStorage) {
+  constructor(selectorElm: HTMLElement, wbSession: WBSession) {
     this.selectorElm = selectorElm
-    this.wbStorage = wbStorage
+    this.wbSession = wbSession
     this.progressBarDom = this.selectorElm.querySelector('#wbc-progress-bar')
 
     // Event Listeners
     window.addEventListener('mouseover', e => {
-      if (this.isStarted && e.target instanceof HTMLElement) {
+      if (this.wbSession.wbState === 'select' && e.target instanceof HTMLElement) {
+        this.hoverElm = e.target
         this.setBoundingRectPos()
       }
+    })
+
+    window.addEventListener('mousedown', e => {
+      this.triggerTimeout = window.setTimeout(() => {
+        if (e.target instanceof HTMLElement) {
+          this.hoverElm = e.target
+          this.setBoundingRectPos()
+        }
+        this.start()
+      }, 1000)
+    })
+
+    window.addEventListener('mouseup', e => {
+      window.clearTimeout(this.triggerTimeout)
     })
 
     // When stop moving a pointer,
     // scanning effect starts after an interval.
     window.addEventListener('mousemove', e => {
-      if (!this.isStarted) return
+      clearTimeout(this.triggerTimeout)
 
-      this.progressBarDom.classList.remove('expand')
-      clearTimeout(this.mouseMoveTimeout)
-      this.mouseMoveTimeout = window.setTimeout(() => {
-        this.progressBarDom.classList.add('expand')
-      }, 700)
+      if (this.wbSession.wbState === 'select') {
+        this.startScanning()
+      }
     })
   }
 
+  private startScanning() {
+    this.progressBarDom.classList.remove('expand')
+    clearTimeout(this.mouseMoveTimeout)
+    this.mouseMoveTimeout = window.setTimeout(() => {
+      this.progressBarDom.classList.add('expand')
+    }, 700)
+  }
+
   private setBoundingRectPos() {
-    let selectedElm = this.wbStorage.getSelectedElement()
-    let boundingRect = selectedElm.getBoundingClientRect()
-    this.selectorElm.style.left = boundingRect.left + 'px'
-    this.selectorElm.style.top = boundingRect.top + 'px'
-    this.selectorElm.style.width = boundingRect.width + 'px'
-    this.selectorElm.style.height = boundingRect.height + 'px'
+    const rect = this.hoverElm.getBoundingClientRect()
+    this.selectorElm.style.left = rect.left + 'px'
+    this.selectorElm.style.top = rect.top + 'px'
+    this.selectorElm.style.width = rect.width + 'px'
+    this.selectorElm.style.height = rect.height + 'px'
   }
 
-  start() {
+  private start() {
+    if (!this.isScanCompleteSet) {
+      console.error('whenScanningCompleted method is not set!')
+      return
+    }
+
+    if (this.wbSession.wbState !== 'pending') {
+      return
+    }
+
     this.selectorElm.classList.remove('hidden')
-    this.isStarted = true
+    this.wbSession.wbState = 'select'
   }
 
-  end() {
+  private stop() {
     this.selectorElm.classList.add('hidden')
-    this.isStarted = false
+    this.wbSession.wbState = 'pending'
   }
 
   whenScanningCompleted(switchMode: Function) {
+    this.isScanCompleteSet = true
+
     this.progressBarDom.addEventListener('transitionend', () => {
+      this.wbSession.setOriginal(this.hoverElm)
+      this.stop()
       switchMode()
     })
   }
