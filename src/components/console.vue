f65e293c6f0de0029f0d0f6b8d0813ae245a241e
Jang Haemin <io@jhaemin.com>
2019-05-06 18:44:58 +0900

    add

---
+++ src/components/console.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="wbc-console">
+
+  </div>
+</template>
+
+<style lang="scss">
+#wbc-console {
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  background-color: rgba(0,0,0,0.7);
+  z-index: 2147483646;
+  overflow-x: hidden;
+  overflow-y: auto;
+}
+</style>
