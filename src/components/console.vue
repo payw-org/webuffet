5258b4db6ff0262e535c9312b3373d182f415314
Jang Haemin <io@jhaemin.com>
2019-05-09 13:11:14 +0900

    Upgrade WEBuffet console

--- src/components/console.vue
+++ src/components/console.vue
@@ -1,18 +1,20 @@
 <template>
-  <div id="wbc-console" :class="{hidden: isHidden}">
-    <button class="close" @click="close">
-      <div class="cross cross-1"></div>
-      <div class="cross cross-2"></div>
+  <div id="wbc-console" :class="{'wb-console-hidden': isHidden}">
+    <button class="wb-close" @click="stop">
+      <div class="wb-cross cross-1"></div>
+      <div class="wb-cross cross-2"></div>
     </button>
-    <div class="hello">
-      <h1 class="title"><span class="wb">WEBuffet</span> <span class="hall">Hall</span></h1>
-      <p class="current-url">{{ currentURL }}</p>
+    <div class="wb-hello">
+      <h1 class="wb-title"><span class="wb">WEBuffet</span> <span class="wb-hall">Hall</span></h1>
+      <p class="wb-current-url">{{ currentURL }}</p>
     </div>
-    <div class="tools"></div>
-    <div class="current-url-cooked"></div>
-    <dir class="other-url-cooked">
-
-    </dir>
+    <div class="wb-current-url-cooked">
+      <div class="wb-item new" @click="startScanning">
+        <div class="wb-cross cross-1"></div>
+        <div class="wb-cross cross-2"></div>
+      </div>
+    </div>
+    <dir class="wb-other-url-cooked"></dir>
   </div>
 </template>
 
@@ -30,18 +32,28 @@ export default {
   },
   methods: {
     start() {
-
+      this.show()
     },
-    close() {
+    stop() {
+      this.hide()
+      document.dispatchEvent(new CustomEvent('consolestop'))
+    },
+    show() {
+      this.$el.getBoundingClientRect().height
+      this.isHidden = false
+    },
+    hide() {
       this.isHidden = true
-      let e = new CustomEvent('closeconsole')
-      document.dispatchEvent(e)
+    },
+    startScanning() {
+      this.hide()
+      document.dispatchEvent(new CustomEvent('startselector'))
     }
   },
   mounted() {
-    setTimeout(() => {
-      this.isHidden = false
-    }, 100)
+    document.addEventListener('loadconsole', () => {
+      this.start()
+    })
   }
 }
 </script>
@@ -56,7 +68,7 @@ export default {
   right: 0;
   left: 0;
   bottom: 0;
-  background-color: #222;
+  background-color: rgba(#111, 0.9);
   z-index: 2147483646;
   overflow-x: hidden;
   overflow-y: auto;
@@ -66,6 +78,7 @@ export default {
   will-change: opacity, transform;
   word-wrap: break-word;
   word-break: break-all;
+
   &, & * {
     outline: none;
   }
@@ -81,45 +94,50 @@ export default {
     line-height: 1;
   }
 
-  &.hidden {
-    transform: scale(1.1);
+  &.wb-console-hidden {
+    transform: scale(1.05);
     opacity: 0;
     pointer-events: none;
   }
 
-  .close {
+  .wb-cross {
+    width: 23px;
+    height: 4px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    background-color: #cfcfcf;
+    border-radius: 50px;
+  
+    &.cross-1 {
+      transform: translateX(-50%) translateY(-50%) rotate(45deg);
+    }
+    &.cross-2 {
+      transform: translateX(-50%) translateY(-50%) rotate(-45deg);
+    }
+  }
+
+  .wb-close {
     position: absolute;
     right: 50px;
     top: 50px;
-    background-color: #333;
+    background-color: #000;
     border: none;
     border-radius: 50px;
     width: 50px;
     height: 50px;
 
-    .cross {
-      width: 23px;
-      height: 3px;
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      background-color: #c3c3c3;
-      border-radius: 50px;
-
-      &.cross-1 {
-        transform: translateX(-50%) translateY(-50%) rotate(45deg);
-      }
-      &.cross-2 {
-        transform: translateX(-50%) translateY(-50%) rotate(-45deg);
-      }
+    &, & * {
+      cursor: pointer !important;
     }
   }
 
-  .hello {
+  .wb-hello {
     padding: 0 50px;
     margin-top: 50px;
 
-    .title {
+    .wb-title {
+      text-align: left;
       font-family: inherit;
       font-size: 40px;
       font-weight: 400;
@@ -128,19 +146,61 @@ export default {
       .wb {
         font-weight: 700;
       }
-      .hall {
+      .wb-hall {
         font-weight: 300;
         color: #cacaca;
       }
     }
 
-    .current-url {
+    .wb-current-url {
       margin: 0;
       margin-top: 1em;
       font-family: inherit;
       font-size: 17px;
       line-height: 1.2;
       color: #5cf5cd;
+    }
+  }
+
+  .wb-current-url-cooked {
+    margin-top: 50px;
+
+    .wb-item {
+      width: 200px;
+      height: 150px;
+      margin: 0 50px;
+      position: relative;
+      box-shadow: 0 10px 50px rgba(0,0,0,0.5);
+      transition: transform 0.2s ease;
+      will-change: transform;
+
+      &:hover {
+        transform: scale(1.05);
+      }
+
+      &:active {
+        transform: scale(0.95);
+      }
+
+      &.new {
+        background-image: linear-gradient(to bottom, #888, #444);
+        border-radius: 8px;
+        &, & * {
+          cursor: pointer !important;
+        }
+
+        .wb-cross {
+          width: 55px;
+          height: 8px;
+
+          &.cross-1 {
+            transform: translateX(-50%) translateY(-50%) rotate(90deg);
+          }
+          &.cross-2 {
+            transform: translateX(-50%) translateY(-50%);
+          }
+        }
+      }
     }
   }
 }
