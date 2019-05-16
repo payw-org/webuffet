90debeb3c848bf6771ba14c832c2274f79f5efc6
Donggyu LIM <dglim27@gmail.com>
2019-05-16 23:10:39 +0900

    New Feature #2 - Webuffet will load saved changes; but only for id-elements

--- src/HideHtml.ts
+++ src/HideHtml.ts
@@ -1,28 +1,26 @@
 import Ruler from "./Ruler"
 
-let html = document.querySelector('html')
-html.style.display = 'none'
+let body = document.querySelector('html')
+body.style.display = 'hidden'
 
 /**
  * Codes read from chrome.storage.sync
  * Attach Style Sheet from read elements
  */
 
-let objects : any [] = []
-
 chrome.storage.sync.get(['myCustom'], function(items) {
-    for(let key in items.myCustom) {
-        console.log(items.myCustom[key].name)
-        objects.push(JSON.parse(JSON.stringify(items.myCustom[key])))
+    /**
+     * Here, Attach Style Sheet from object in items
+     * Get URL first, check the URL matches with document.URL
+     * If matches, find elements in document with name and generate CSS for that element with style
+     */
+    if(items.myCustom[0] === {}) return
+    else {
+        for(let key in items.myCustom) {
+            let item = items.myCustom[key]
+            document.getElementById(item.name.id).style.transform = Ruler.generateCSS(item.style.translatex, item.style.translatey, item.style.scale, item.style.rotate)
+        }
     }
 })
 
-console.log(objects[0])
-let job = function() {
-    for (let idx in objects) {
-        let item = objects[idx]
-        document.getElementById(item.name.id).style.transform = Ruler.generateCSS(item.style.translatex, item.style.translatey, item.style.scale, item.style.rotate)
-    }
-}
-
-html.style.display = ''
+body.style.display = 'visible'
