fca96a23e1d4ae84686152bd9270d21b2ddf3435
SangHyune546 <parksh9014@gmail.com>
2019-05-19 19:03:56 +0900

    Add json-rpc protocol logic with Crypto

--- extension/popup.js
+++ extension/popup.js
@@ -1,11 +1,19 @@
 
   document.addEventListener('DOMContentLoaded', function(){
+
+    var login_Info;
     var login_button = document.getElementById("login_button");   
      login_button.addEventListener("click", function(){
 
         chrome.identity.getProfileUserInfo(function(userInfo) {
 
-            console.log(JSON.stringify(userInfo));
+
+            console.log("Email : " +userInfo.email);
+            console.log("ID : " +userInfo.id);
+
+            login_Info = JSON.stringify(userInfo);
+
+            console.log(login_Info);
 
           });
     } 
