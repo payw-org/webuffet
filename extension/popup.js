250c7d81dbe8487e25a6e9be3f541669bb144a39
SangHyune546 <parksh9014@gmail.com>
2019-05-19 23:14:06 +0900

    Chrome identity Information to Json-rpc server & call back login information

--- extension/popup.js
+++ extension/popup.js
@@ -1,21 +1,41 @@
+$(function() {
 
-  document.addEventListener('DOMContentLoaded', function(){
+  $.jsonRPC.setup({
+      endPoint : 'http://localhost:3000/api',
+      namespace : ''
+  });
+  
+  $("#login_button").click(function() {
 
-    var login_Info;
-    var login_button = document.getElementById("login_button");   
-     login_button.addEventListener("click", function(){
+      chrome.identity.getProfileUserInfo(function(userInfo) {
+        var UserEmail, UserID;
+          UserEmail = JSON.stringify(userInfo.email);
+          UserID = JSON.stringify(userInfo.id);
+      console.log(UserEmail+"/"+UserID);
 
-        chrome.identity.getProfileUserInfo(function(userInfo) {
+      var method = 'echo';
+		  $.jsonRPC.request(method, {
+			id: 1001,
+			params: [UserEmail,UserID],
+			success: function(data) {
+				println('정상 응답을 받았습니다.');
+				console.dir(data);
+				
+				println(data.result);
+			},
+			error: function(data) {
+				println('에러 응답을 받았습니다.');
+				console.dir(data);
+				
+				println(data.error.message);
+			}
+		});
+		println('[' + method + '] method로 요청을 보냈습니다.');
 
-
-            console.log("Email : " +userInfo.email);
-            console.log("ID : " +userInfo.id);
-
-            login_Info = JSON.stringify(userInfo);
-
-            console.log(login_Info);
-
-          });
-    } 
-);
+    });
+  });  
 });
+
+function println(data) {
+  $("#results").append('<p>' + data + '</p>');
+}
