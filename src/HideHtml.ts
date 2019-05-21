d101a60a5ee62ae14f0e891b9d7ea03b16d374e9
Donggyu LIM <dglim27@gmail.com>
2019-05-21 17:23:11 +0900

    Capture original image and inject the source to body

--- src/HideHtml.ts
+++ src/HideHtml.ts
@@ -1,4 +1,5 @@
 import Ruler from "./Ruler"
+import html2canvas from 'html2canvas'
 
 let body = document.querySelector('html')
 body.style.visibility = 'hidden'
@@ -16,32 +17,37 @@ window.onload = () => {
         * If matches, find elements in document with name and generate CSS for that element with style
         */
         if(items.myCustom[0] === {}) return
+        
         else {
+            let srcElm = document.createElement('div')
+            srcElm.id = 'webuffet-image-sources'
+            let imgSrcArr: Array<string> = []
+            document.body.appendChild(srcElm)
             for(let key in items.myCustom) {
                 let item = items.myCustom[key]
+                let element
                 if(item.url != document.URL) continue;
                 
                 if(item.name.id != "") {
-                    if(item.style.isDeleted == true) {
-                        document.getElementById(item.name.id).style.display = 'none'
-                    } else {
-                        document.getElementById(item.name.id).style.transform = Ruler.generateCSS(item.style.translatex, item.style.translatey, item.style.scale, item.style.rotate)
-                    }   
+                    element = document.getElementById(item.name.id)
                 } else if(item.name.cName != "") {
-                    let element = document.getElementsByClassName(item.name.cName).item(item.name.cIndex) as HTMLElement
-                    if(item.style.isDeleted == true) {
-                        element.style.display = 'none'
-                    } else {
-                        element.style.transform = Ruler.generateCSS(item.style.translatex, item.style.translatey, item.style.scale, item.style.rotate)
-                    }
+                    element = document.getElementsByClassName(item.name.cName).item(item.name.cIndex) as HTMLElement
                 } else {
-                    let element : any = document.getElementsByTagName(item.name.tName).item(item.name.tIndex)
-                    if(item.style.isDeleted == true) {
-                        element.style.display = 'none'
-                    } else {
-                        element.style.transform = Ruler.generateCSS(item.style.translatex, item.style.translatey, item.style.scale, item.style.rotate)
-                    }
+                    element = document.getElementsByTagName(item.name.tName).item(item.name.tIndex) as HTMLElement
                 }
+                html2canvas(element, {
+                    useCORS: true,
+                    backgroundColor: null,
+                  }).then((canvas: HTMLCanvasElement) => {
+                    imgSrcArr.push(canvas.toDataURL('image/png'))
+                    srcElm.setAttribute('data', JSON.stringify(imgSrcArr))
+                })
+
+                if(item.style.isDeleted == true) {
+                    element.style.display = 'none'
+                } else {
+                    element.style.transform = Ruler.generateCSS(item.style.translatex, item.style.translatey, item.style.scale, item.style.rotate)
+                }   
             }
         }
     })
