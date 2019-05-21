460db292940dfd74380c4fc033b39e60e618ff02
Donggyu LIM <dglim27@gmail.com>
2019-05-22 02:05:11 +0900

    bug fix

--- src/WBApexTool.ts
+++ src/WBApexTool.ts
@@ -340,7 +340,6 @@ export default class WBApexTool {
             } else break
           }
           captures.push(this.wbSession.getOriginalState().imgSrc)
-          console.log(captures)
           document.body.removeChild(document.getElementById('webuffet-image-sources'))
           let srcElm = document.createElement('div')
           srcElm.id = 'webuffet-image-sources'
@@ -349,13 +348,19 @@ export default class WBApexTool {
         })
       } else {
         captures = JSON.parse(document.querySelector('#webuffet-image-sources').getAttribute('data'))
-        captures.push(this.wbSession.getOriginalState().imgSrc)
-        console.log(captures)
-        document.body.removeChild(document.getElementById('webuffet-image-sources'))
-        let srcElm = document.createElement('div')
-        srcElm.id = 'webuffet-image-sources'
-        srcElm.setAttribute('data', JSON.stringify(captures))
-        document.body.appendChild(srcElm)
+        chrome.storage.sync.get(['myCustom'], item => {
+          for(let i = 0; i < item.myCustom.length; i++) {
+            if(item.myCustom[i].url != document.URL) {
+              captures.splice(i, 0, 'null')
+            } else break
+          }
+          captures.push(this.wbSession.getOriginalState().imgSrc)
+          document.body.removeChild(document.getElementById('webuffet-image-sources'))
+          let srcElm = document.createElement('div')
+          srcElm.id = 'webuffet-image-sources'
+          srcElm.setAttribute('data', JSON.stringify(captures))
+          document.body.appendChild(srcElm)
+        })
       }
       // captures.push(this.wbSession.getOriginalState().imgSrc)
       // console.log(captures)
