075d6a13f5de42a034773c49ccf28ba68a158b03
Jongwoo Na <njw1204@naver.com>
2019-06-06 00:31:28 +0900

    Fix error that some page become blank

--- src/HideHtml.ts
+++ src/HideHtml.ts
@@ -18,65 +18,70 @@ window.onload = () => {
         * Get URL first, check the URL matches with document.URL
         * If matches, find elements in document with name and generate CSS for that element with style
         */
-        if(!items.myCustom[0]) {
-            body.style.opacity = '1'
-            return
-        }
-        
-        else {
-            let srcElm = document.createElement('div')
-            srcElm.id = 'webuffet-image-sources'
-            document.body.appendChild(srcElm)
-            let imgSrcArr: Array<String> = []
 
-            let processElement = function (items: any, i: number) {
-                if (i == items.myCustom.length) {
-                    // body.style.transform = ''
-                    body.style.opacity = '1'
-                    return
-                }
-                let num : number = i
-                let item = items.myCustom[i]
-                let element: HTMLElement
-                if(item.url != document.URL) {
-                    if (i > items.myCustom.length - 1) {
-                        body.style.opacity = '1'
-                    }
-                    imgSrcArr.push('null')
-                    srcElm.setAttribute('data', JSON.stringify(imgSrcArr))
-                    processElement(items, i+1)
-                    return
-                }
-                
-                
-                if(item.name.id != "") {
-                    element = document.getElementById(item.name.id)
-                } else if(item.name.cName != "") {
-                    element = document.getElementsByClassName(item.name.cName).item(item.name.cIndex) as HTMLElement
-                } else {
-                    element = document.getElementsByTagName(item.name.tName).item(item.name.tIndex) as HTMLElement
-                }
-                
-                html2canvas(element, {
-                    useCORS: true,
-                    backgroundColor: null,
-                  }).then((canvas: HTMLCanvasElement) => {
-                    imgSrcArr[num] = canvas.toDataURL('image/png')
-                    srcElm.setAttribute('data', JSON.stringify(imgSrcArr))
-                    if(item.style.isDeleted == true) {
-                        element.style.display = 'none'
-                    } else {
-                        element.style.transform = Ruler.generateCSS(item.style.translatex, item.style.translatey, item.style.scale, item.style.rotate)
-                    }
-                    if(i > items.myCustom.length - 1) {
-                        body.style.opacity = '1'
-                    }
-
-                    processElement(items, i + 1)
-                })
+        try {
+            if(!items.myCustom[0]) {
+                body.style.opacity = '1'
+                return
             }
 
-            processElement(items, 0)
+            else {
+                let srcElm = document.createElement('div')
+                srcElm.id = 'webuffet-image-sources'
+                document.body.appendChild(srcElm)
+                let imgSrcArr: Array<String> = []
+
+                let processElement = function (items: any, i: number) {
+                    if (i == items.myCustom.length) {
+                        // body.style.transform = ''
+                        body.style.opacity = '1'
+                        return
+                    }
+                    let num : number = i
+                    let item = items.myCustom[i]
+                    let element: HTMLElement
+                    if(item.url != document.URL) {
+                        if (i > items.myCustom.length - 1) {
+                            body.style.opacity = '1'
+                        }
+                        imgSrcArr.push('null')
+                        srcElm.setAttribute('data', JSON.stringify(imgSrcArr))
+                        processElement(items, i+1)
+                        return
+                    }
+
+
+                    if(item.name.id != "") {
+                        element = document.getElementById(item.name.id)
+                    } else if(item.name.cName != "") {
+                        element = document.getElementsByClassName(item.name.cName).item(item.name.cIndex) as HTMLElement
+                    } else {
+                        element = document.getElementsByTagName(item.name.tName).item(item.name.tIndex) as HTMLElement
+                    }
+
+                    html2canvas(element, {
+                        useCORS: true,
+                        backgroundColor: null,
+                    }).then((canvas: HTMLCanvasElement) => {
+                        imgSrcArr[num] = canvas.toDataURL('image/png')
+                        srcElm.setAttribute('data', JSON.stringify(imgSrcArr))
+                        if(item.style.isDeleted == true) {
+                            element.style.display = 'none'
+                        } else {
+                            element.style.transform = Ruler.generateCSS(item.style.translatex, item.style.translatey, item.style.scale, item.style.rotate)
+                        }
+                        if(i > items.myCustom.length - 1) {
+                            body.style.opacity = '1'
+                        }
+
+                        processElement(items, i + 1)
+                    })
+                }
+
+                processElement(items, 0)
+            }
+        } finally {
+            body.style.opacity = '1'
         }
     })
 }
